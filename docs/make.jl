b9e7259670399308ea5756741316d6f1ba5ba9dc
ashwani rathee <ab669522@gmail.com>
2023-06-30 19:43:40 +0530

    Updates!

--- docs/make.jl
+++ docs/make.jl
@@ -3,25 +3,8 @@ using MultiDocumenter
 clonedir = mktempdir()
 
 docs = [
-    MultiDocumenter.MultiDocRef(
-        upstream = joinpath(clonedir, "ImageFiltering"),
-        path = "filter",
-        name = "ImageFiltering",
-        giturl = "https://github.com/JuliaImages/ImageFiltering.jl.git",
-    ),
-    MultiDocumenter.MegaDropdownNav("Packages", [
-
-        MultiDocumenter.Column("High-Level Packages", [
-            # DitherPunk.jl provides various image dithering operations.
-            # ImageBinarization.jl provides various image binarization algorithms.
-            # ImageContrastAdjustment.jl* supports image contrast enhancement and manipulation.
-            # ImageMorphology.jl* provides several morphological operations for image processing.
-            # ImageFiltering.jl* supports basic filtering operations.
-            # ImageFeatures.jl is a package for identifying and characterizing "keypoints" (salient features) in images.
-            # ImageQualityIndexes.jl* provides several image quality assessment indexes, e.g., PSNR and SSIM.
-            # ImageTransformations.jl* provides functions related to geometric transformations.
-            # ImageSegmentation.jl provides several image segmentation algorithms.
-            # ImageInpainting.jl provides image inpainting algorithms in Julia
+    MultiDocumenter.MegaDropdownNav("Mega Debugger", [
+        MultiDocumenter.Column("High Level Packages", [
             MultiDocumenter.MultiDocRef(
                 upstream = joinpath(clonedir, "ImageFiltering"),
                 path = "filter",
@@ -34,183 +17,33 @@ docs = [
                 name = "DitherPunk",
                 giturl = "https://github.com/JuliaImages/DitherPunk.jl.git",
             ),
-            MultiDocumenter.MultiDocRef(
-                upstream = joinpath(clonedir, "ImageBinarization.jl"),
-                path = "binarize",
-                name = "ImageBinarization",
-                giturl = "https://github.com/JuliaImages/ImageBinarization.jl.git",
-            ),
-            MultiDocumenter.MultiDocRef(
-                upstream = joinpath(clonedir, "ImageContrastAdjustment.jl"),
-                path = "contrast",
-                name = "ImageContrastAdjustment",
-                giturl = "https://github.com/JuliaImages/ImageContrastAdjustment.jl.git"
-            ),
-            MultiDocumenter.MultiDocRef(
-                upstream = joinpath(clonedir, "ImageMorphology.jl"),
-                path = "morphology",
-                name = "ImageMorphology",
-                giturl = "https://github.com/JuliaImages/ImageMorphology.jl.git"
-            ),
-            MultiDocumenter.MultiDocRef(
-                upstream = joinpath(clonedir, "ImageFeatures.jl"),
-                path = "features",
-                name = "ImageFeatures",
-                giturl = "https://github.com/JuliaImages/ImageFeatures.jl.git"
-            ),
-
-            # MultiDocumenter.MultiDocRef(
-            #     upstream = joinpath(clonedir, "ImageQualityIndexes.jl"),
-            #     path = "debug",
-            #     name = "ImageQualityIndexes",
-            #     giturl = "https://github.com/JuliaImages/ImageQualityIndexes.jl.git"
-            # # ),
-            # MultiDocumenter.MultiDocRef(
-            #     upstream = joinpath(clonedir, "ImageTransformations.jl"),
-            #     path = "debug",
-            #     name = "ImageTransformations",
-            #     giturl = "https://github.com/JuliaImages/ImageTransformations.jl.git"
-            # ),
-            # MultiDocumenter.MultiDocRef(
-            #     upstream = joinpath(clonedir, "ImageSegmentation.jl"),
-            #     path = "debug",
-            #     name = "ImageSegmentation",
-            #     giturl = "https://github.com/JuliaImages/ImageSegmentation.jl.git"
-            # ),
-            # MultiDocumenter.MultiDocRef(
-            #     upstream = joinpath(clonedir, "ImageInpainting.jl"),
-            #     path = "debug",
-            #     name = "ImageInpainting",
-            #     giturl = "https://github.com/JuliaImages/ImageInpainting.jl.git"
-            # ),
         ]),
-        # MultiDocumenter.Column("Traits and Utilities", [
-        #     # ImageAxes.jl supports AxisArrays.jl* to endow the axes with "meaning".
-        #     # ImageMetadata.jl is a simple package providing utilities for working with images that have metadata attached.
-        #     # ImageDraw.jl let you draw shapes on an image.
-        #     # ImageDistances.jl is a Distances.jl wrapper for images.
-        #     # OffsetArrays.jl supports arrays with arbitrary indices offsets.
-        #     # MappedViews.jl provides lazy in-place transformations of arrays.
-        #     # PaddedViews.jl add virtual padding to the edges of an array. It also allows you to composite multiple images together.
-        #     # TestImages.jl provides several "standard" test images.
-        #     MultiDocumenter.MultiDocRef(
-        #         upstream = joinpath(clonedir, "ImageAxes.jl"),
-        #         path = "inf",
-        #         name = "ImageAxes",
-        #         giturl = "https://github.com/JuliaImages/ImageAxes.jl.git",
-        #     ),
-        #     MultiDocumenter.MultiDocRef(
-        #         upstream = joinpath(clonedir, "ImageMetadata.jl"),
-        #         path = "debug",
-        #         name = "ImageMetadata",
-        #         giturl = "https://github.com/JuliaImages/ImageMetadata.jl.git",
-        #     ),
-        #     MultiDocumenter.MultiDocRef(
-        #         upstream = joinpath(clonedir, "ImageDraw.jl"),
-        #         path = "debug",
-        #         name = "ImageDraw",
-        #         giturl = "https://github.com/JuliaImages/ImageDraw.jl.git",
-        #     ),
-        #     # MultiDocumenter.MultiDocRef(
-        #     #     upstream = joinpath(clonedir, "ImageDistances.jl"),
-        #     #     path = "debug",
-        #     #     name = "ImageDistances",
-        #     #     giturl = "https://github.com/JuliaImages/ImageDistances.jl.git",
-        #     # ),
-        #     # MultiDocumenter.MultiDocRef(
-        #     #     upstream = joinpath(clonedir, "OffsetArrays.jl"),
-        #     #     path = "debug",
-        #     #     name = "OffsetArrays",
-        #     #     giturl = "https://github.com/JuliaArrays/OffsetArrays.jl.git",
-        #     # ),
-        #     # MultiDocumenter.MultiDocRef(
-        #     #     upstream = joinpath(clonedir, "MappedArrays.jl"),
-        #     #     path = "debug",
-        #     #     name = "MappedArrays",
-        #     #     giturl = "https://github.com/JuliaArrays/MappedArrays.jl.git",
-        #     # ),
-        #     # MultiDocumenter.MultiDocRef(
-        #     #     upstream = joinpath(clonedir, "PaddedViews.jl"),
-        #     #     path = "debug",
-        #     #     name = "PaddedViews",
-        #     #     giturl = "https://github.com/JuliaArrays/PaddedViews.jl.git",
-        #     # ),
-        #     MultiDocumenter.MultiDocRef(
-        #         upstream = joinpath(clonedir, "TestImages.jl"),
-        #         path = "debug",
-        #         name = "TestImages",
-        #         giturl = "https://github.com/JuliaImages/TestImages.jl.git",
-        #     ),
-        #     # ImageShow is used to support image display in Juno and IJulia. This happens automatically if you are using Images.
-        #     # ImageInTerminal is used to support image display in terminal.
-        #     # ImageView is an image display GUI. (For OSX and Windows platforms, Julia at least v1.3 is required)
-        #     # MultiDocumenter.MultiDocRef(
-        #     #     upstream = joinpath(clonedir, "ImageShow.jl"),
-        #     #     path = "debug",
-        #     #     name = "ImageShow",
-        #     #     giturl = "https://github.com/JuliaImages/ImageShow.jl.git",
-        #     # ),
-        #     # MultiDocumenter.MultiDocRef(
-        #     #     upstream = joinpath(clonedir, "ImageInTerminal.jl"),
-        #     #     path = "debug",
-        #     #     name = "ImageInTerminal",
-        #     #     giturl = "https://github.com/JuliaImages/ImageInTerminal.jl.git",
-        #     # ),
-        #     # MultiDocumenter.MultiDocRef(
-        #     #     upstream = joinpath(clonedir, "ImageView.jl"),
-        #     #     path = "debug",
-        #     #     name = "ImageView",
-        #     #     giturl = "https://github.com/JuliaImages/ImageView.jl.git",
-        #     # ),
-
-                    
-        # ]),
-        # ColorTypes.jl*, Colors.jl* and ColorVectorSpace.jl* provides pixel-level definitions and functions.
-        # FixedPointNumbers.jl* provides several data types (e.g., N0f8) for image storage usage.
-        # ImageCore.jl* provides various basic and convenient views, traits and functions to support image processing algorithms.
-        # ImageBase.jl
-        # MultiDocumenter.Column("Low Level Packages",[
-        #     # MultiDocumenter.MultiDocRef(
-        #     #     upstream = joinpath(clonedir, "ColorTypes.jl"),
-        #     #     path = "debug",
-        #     #     name = "ColorTypes",
-        #     #     giturl = "https://github.com/JuliaColors/ColorTypes.jl.git",
-        #     # ),
-        #     MultiDocumenter.MultiDocRef(
-        #         upstream = joinpath(clonedir, "Colors.jl"),
-        #         path = "debug",
-        #         name = "Colors",
-        #         giturl = "https://github.com/JuliaGraphics/Colors.jl.git",
-        #     ),
-        #     # MultiDocumenter.MultiDocRef(
-        #     #     upstream = joinpath(clonedir, "ColorVectorSpace.jl"),
-        #     #     path = "debug",
-        #     #     name = "ColorVectorSpace",
-        #     #     giturl = "https://github.com/JuliaColors/ColorVectorSpace.jl.git",
-        #     # ),
-        #     # MultiDocumenter.MultiDocRef(
-        #     #     upstream = joinpath(clonedir, "FixedPointNumbers.jl"),
-        #     #     path = "debug",
-        #     #     name = "FixedPointNumbers",
-        #     #     giturl = "https://github.com/JuliaMath/FixedPointNumbers.jl.git",
-        #     # ),
-        #     # MultiDocumenter.MultiDocRef(
-        #     #     upstream = joinpath(clonedir, "ImageCore.jl"),
-        #     #     path = "debug",
-        #     #     name = "ImageCore",
-        #     #     giturl = "https://github.com/JuliaImages/ImageCore.jl.git",
-        #     # ),
-        #     # MultiDocumenter.MultiDocRef(
-        #     #     upstream = joinpath(clonedir, "ImageBase.jl"),
-        #     #     path = "debug",
-        #     #     name = "ImageBase",
-        #     #     giturl = "https://github.com/JuliaImages/ImageBase.jl.git",
-        #     # ),
-        # ]),
+        MultiDocumenter.Column("Visualisation", [
+            MultiDocumenter.MultiDocRef(
+                upstream = joinpath(clonedir, "ImageView"),
+                path = "imgview",
+                name = "ImageView",
+                giturl = "https://github.com/JuliaImages/ImageView.jl.git",
+            ),
+            MultiDocumenter.MultiDocRef(
+                upstream = joinpath(clonedir, "ImageInTerminal"),
+                path = "imginterminal",
+                name = "ImageInTerminal",
+                giturl = "https://github.com/JuliaImages/ImageInTerminal.jl.git",
+            ),
+        ]),
     ]),
+    MultiDocumenter.MultiDocRef(
+        upstream = joinpath(clonedir, "DataSets"),
+        path = "data",
+        name = "DataSets",
+        giturl = "https://github.com/JuliaComputing/DataSets.jl.git",
+        # or use ssh instead for private repos:
+        # giturl = "git@github.com:JuliaComputing/DataSets.jl.git",
+    ),
 ]
 
-outpath = joinpath(@__DIR__, "Overview")
+outpath = joinpath(@__DIR__, "out")
 
 MultiDocumenter.make(
     outpath,
@@ -218,5 +51,5 @@ MultiDocumenter.make(
     search_engine = MultiDocumenter.SearchConfig(
         index_versions = ["stable"],
         engine = MultiDocumenter.FlexSearch
-    ),
+    )
 )
