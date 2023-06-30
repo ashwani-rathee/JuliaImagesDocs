f2d7f9baff61e777f49a5a1a0b8d261563b1c08e
ashwani rathee <ab669522@gmail.com>
2023-06-30 22:56:41 +0530

    Remove unneccessary

--- docs/out/features/versions.js
+++
@@ -1,6 +0,0 @@
-var DOC_VERSIONS = [
-  "stable",
-  "v0.4",
-  "v0.3",
-  "dev",
-];
--- docs/out/transformations/versions.js
+++
@@ -1,8 +0,0 @@
-var DOC_VERSIONS = [
-  "stable",
-  "v0.9",
-  "v0.8",
-  "dev",
-];
-var DOCUMENTER_NEWEST = "v0.9.5";
-var DOCUMENTER_STABLE = "stable";
