e17f2b15156f4e785c6d51918805bd779a1128c6
ashwani rathee <ab669522@gmail.com>
2023-06-30 22:46:16 +0530

    Website updates!

--- docs/make.jl
+++ docs/make.jl
@@ -20,19 +20,33 @@ docs = [
                 name = "ImageFiltering",
                 giturl = "https://github.com/JuliaImages/ImageFiltering.jl.git",
             ),
-
+            # ImageCorners.jl
             MultiDocumenter.MultiDocRef(
-                upstream = joinpath(clonedir, "DitherPunk"),
-                path = "dither",
-                name = "DitherPunk",
-                giturl = "https://github.com/JuliaImages/DitherPunk.jl.git",
+                upstream = joinpath(clonedir, "ImageCorners"),
+                path = "corners",
+                name = "ImageCorners",
+                giturl = "https://github.com/JuliaImages/ImageCorners.jl.git",
             ),
-            # QRCoders.jl
+            # ColorQuantization.jl
             MultiDocumenter.MultiDocRef(
-                upstream = joinpath(clonedir, "QRCoders"),
-                path = "qrcoders",
-                name = "QRCoders",
-                giturl = "https://github.com/JuliaImages/QRCoders.jl.git",
+                upstream = joinpath(clonedir, "ColorQuantization"),
+                path = "quantization",
+                name = "ColorQuantization",
+                giturl = "https://github.com/JuliaImages/ColorQuantization.jl.git",
+            ),
+            # OpenCV.jl
+            MultiDocumenter.MultiDocRef(
+                upstream = joinpath(clonedir, "OpenCV"),
+                path = "opencv",
+                name = "OpenCV",
+                giturl = "https://github.com/JuliaImages/OpenCV.jl.git",
+            ),
+            # ImageSmooth
+            MultiDocumenter.MultiDocRef(
+                upstream = joinpath(clonedir, "ImageSmooth"),
+                path = "smooth",
+                name = "ImageSmooth",
+                giturl = "https://github.com/JuliaImages/ImageSmooth.jl.git",
             ),
             # ImageTransformations
             MultiDocumenter.MultiDocRef(
@@ -41,6 +55,23 @@ docs = [
                 name = "ImageTransformations",
                 giturl = "https://github.com/JuliaImages/ImageTransformations.jl.git",
             ),
+
+            # QRCoders.jl
+            # MultiDocumenter.MultiDocRef(
+            #     upstream = joinpath(clonedir, "QRCoders"),
+            #     path = "qrcoders",
+            #     name = "QRCoders",
+            #     giturl = "https://github.com/JuliaImages/QRCoders.jl.git",
+            # ),
+            # ImageTransformations
+            # MultiDocumenter.MultiDocRef(
+            #     upstream = joinpath(clonedir, "ImageTransformations"),
+            #     path = "transformations",
+            #     name = "ImageTransformations",
+            #     giturl = "https://github.com/JuliaImages/ImageTransformations.jl.git",
+            # ),
+
+            # works
             # ImageFeatures
             MultiDocumenter.MultiDocRef(
                 upstream = joinpath(clonedir, "ImageFeatures"),
@@ -49,12 +80,14 @@ docs = [
                 giturl = "https://github.com/JuliaImages/ImageFeatures.jl.git",
             ),
             # ImageBinarization.jl
-            MultiDocumenter.MultiDocRef(
-                upstream = joinpath(clonedir, "ImageBinarization"),
-                path = "binarization",
-                name = "ImageBinarization",
-                giturl = "https://github.com/JuliaImages/ImageBinarization.jl.git",
-            ),
+            # MultiDocumenter.MultiDocRef(
+            #     upstream = joinpath(clonedir, "ImageBinarization"),
+            #     path = "binarization",
+            #     name = "ImageBinarization",
+            #     giturl = "https://github.com/JuliaImages/ImageBinarization.jl.git",
+            # ),
+
+            # works
             # ExifViewer
             MultiDocumenter.MultiDocRef(
                 upstream = joinpath(clonedir, "ExifViewer"),
@@ -63,14 +96,15 @@ docs = [
                 giturl = "https://github.com/JuliaImages/ExifViewer.jl.git",
             ),
             # ImageMorphology
-            MultiDocumenter.MultiDocRef(
-                upstream = joinpath(clonedir, "ImageMorphology"),
-                path = "morphology",
-                name = "ImageMorphology",
-                giturl = "https://github.com/JuliaImages/ImageMorphology.jl.git",
-            ),
+            # MultiDocumenter.MultiDocRef(
+            #     upstream = joinpath(clonedir, "ImageMorphology"),
+            #     path = "morphology",
+            #     name = "ImageMorphology",
+            #     giturl = "https://github.com/JuliaImages/ImageMorphology.jl.git",
+            # ),
         ]),
         MultiDocumenter.Column("Visualisation & Drawing", [
+            # works
             MultiDocumenter.MultiDocRef(
                 upstream = joinpath(clonedir, "ImageDraw"),
                 path = "imgdraw",
@@ -82,7 +116,7 @@ docs = [
 
 ]
 
-outpath = joinpath(@__DIR__, "out")
+outpath = joinpath(@__DIR__, "overview")
 
 MultiDocumenter.make(
     outpath,
@@ -90,5 +124,5 @@ MultiDocumenter.make(
     search_engine = MultiDocumenter.SearchConfig(
         index_versions = ["stable"],
         engine = MultiDocumenter.FlexSearch
-    )
+    ),
 )
