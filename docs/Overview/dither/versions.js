eeb56995285e45502801cdf5c1f627d1a949ec3c
ashwani rathee <ab669522@gmail.com>
2023-06-30 14:48:09 +0530

    Adds prebuilt documentation

---
+++ docs/Overview/dither/versions.js
@@ -0,0 +1,15 @@
+var DOC_VERSIONS = [
+  "stable",
+  "v3.0",
+  "v2.4",
+  "v2.3",
+  "v2.0",
+  "v1.3",
+  "v1.2",
+  "v1.1",
+  "v0.2",
+  "v0.1",
+  "dev",
+];
+var DOCUMENTER_NEWEST = "v3.0.3";
+var DOCUMENTER_STABLE = "stable";
